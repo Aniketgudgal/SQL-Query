d78b8adb7b0216d1c26e75d57b8caab33787a764
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-04-02 12:45:01 +0530

    Function with loop

--- Function/Function_In_SQL.sql
+++ Function/Function_In_SQL.sql
@@ -1,4 +1,7 @@
 use demo_123;
+
+show tables;
+
 delimiter //
 
 create function getSqrt(n int(2)) returns int(2)
@@ -11,3 +14,30 @@ END
 //
 
 select getSqrt(4);
+
+delimiter //
+-- this function is used for the perform the operation of string concatination with loop
+create function printName(name varchar(200)) returns varchar(200)
+DETERMINISTIC
+begin
+DECLARE i int(3);
+SET i = 1;
+
+test_loop: LOOP
+IF i = 5 THEN
+	LEAVE test_loop;
+ELSE
+ SET name = concat(name,name);
+SET i = i+1;
+END IF;
+END LOOP;
+return name;
+END
+//
+
+delimiter ;
+select printName("Aniket")  ;
+
+select * from employee;
+
+drop function printName;
